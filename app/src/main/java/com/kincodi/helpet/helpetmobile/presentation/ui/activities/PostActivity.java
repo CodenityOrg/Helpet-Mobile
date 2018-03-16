346d6d6c899f06cd9061cbc8a7cd7f4a155b897d
julcsar13 <julcsar13@gmail.com>
2018-03-15 21:06:32 -0500

    crear adaptador de lista de post

---
+++ app/src/main/java/com/kincodi/helpet/helpetmobile/presentation/ui/activities/PostActivity.java
@@ -0,0 +1,13 @@
+package com.kincodi.helpet.helpetmobile.presentation.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
